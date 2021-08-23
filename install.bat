774855c45e0502599b6d5ac223776eeb83bf42fc
dsn <dsn@dsnn.io>
2021-08-23 17:16:52 +0200

    remove installer

--- install.bat
+++
@@ -1,5 +0,0 @@
-copy C:\Users\dsn\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json C:\Users\dsn\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json.bak
-
-del C:\Users\dsn\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-
-mklink /H C:\Users\dsn\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json C:\stuff\windows-terminal-settings\settings.json 
